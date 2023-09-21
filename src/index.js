d6b8e5e84e84055d7f4a66c048169bbe887488bb
k4mien <kontakt.karolkaminski@gmail.com>
2023-09-21 15:24:07 +0200

    changed source code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-const { Client, Partials, IntentsBitField } = require("discord.js");
+const { Client, IntentsBitField } = require("discord.js");
 const fs = require("fs");
 const config = require("./config.json");
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.prefix = config.prefix;
+
+client.on('ready', (c)=> {
+    console.log(`${c.user.tag} is online`)
+})
+
+client.login(config.token);
